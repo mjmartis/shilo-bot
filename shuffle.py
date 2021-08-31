8e6727b5acd13708cfb1d4553d2330160db3dc49
Michael Martis <martisdev@gmail.com>
2021-08-31 11:05:40 +0800

    Bot command works

--- shuffle.py
+++ shuffle.py
@@ -1,7 +1,24 @@
 import json
 from discord.ext import commands
 
-CONFIG_FN = 'shuffle.json'
-config = json.loads(open(CONFIG_FN, 'r').read())
+CONFIG_FILE = 'shuffle.json'
 
-print(config['metadata']['token'])
+# Read config.
+
+config = json.loads(open(CONFIG_FILE, 'r').read())
+
+# Define bot.
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} connected to Discord!')
+
+@bot.command(name='test', help='')
+async def test(ctx):
+    await ctx.send('test response')
+
+# Run bot.
+
+bot.run(config['metadata']['token'])
