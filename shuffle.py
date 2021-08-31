7702a894886f747ad0e23a38f86ec04f7b5e975c
Michael Martis <michaeljmartis@gmail.com>
2021-08-31 18:24:37 +0800

    Voice connection works

--- shuffle.py
+++ shuffle.py
@@ -13,27 +13,31 @@ config = json.loads(open(CONFIG_FILE, 'r').read())
 
 bot = commands.Bot(command_prefix='!')
 
+def on_other_channel(ctx):
+    return ctx.author.voice and ctx.voice_client and \
+           ctx.author.voice.channel != ctx.voice_client.channel
+
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name} connected to Discord!')
+    print(f'[INFO] {bot.user.name} connected.')
 
 @bot.command(name='start')
 async def start(ctx):
+    print(f'[INFO] Received start command')
+
+    if on_other_channel(ctx):
+        await ctx.send(f'{bot.user.name} connected to a different channel!')
+        return
+
     dest = ctx.author.voice
 
     if not dest:
         await ctx.send('User not in voice channel!')
         return
 
-    # Move bot if it is already playing.
-    #if ctx.voice_client:
-    #    await ctx.voice_state.voice.move_to(dest)
-    #    return
-
     await dest.channel.connect()
     await ctx.send(f'Joined the voice channel {dest.channel.name}.')
 
-@bot.command(name='test')
 async def test(ctx):
     await ctx.send('test response')
 
