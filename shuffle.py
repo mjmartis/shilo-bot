2f05dcb1968a37bb954b5fa7dca6edc51f980a16
Michael Martis <martisdev@gmail.com>
2021-08-31 12:11:23 +0800

    Initial voice connection

--- shuffle.py
+++ shuffle.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import json
 from discord.ext import commands
 
@@ -15,7 +17,23 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'{bot.user.name} connected to Discord!')
 
-@bot.command(name='test', help='')
+@bot.command(name='start')
+async def start(ctx):
+    dest = ctx.author.voice
+
+    if not dest:
+        await ctx.send('User not in voice channel!')
+        return
+
+    # Move bot if it is already playing.
+    #if ctx.voice_client:
+    #    await ctx.voice_state.voice.move_to(dest)
+    #    return
+
+    await dest.channel.connect() # CONNECTING TO DESTINATION
+    await ctx.send(f'Joined the voice channel {dest.channel.name}.')
+
+@bot.command(name='test')
 async def test(ctx):
     await ctx.send('test response')
 
