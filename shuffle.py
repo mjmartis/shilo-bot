128deb118e0387096e9530899d3bdbda258a43d7
Michael Martis <martisdev@gmail.com>
2021-08-31 10:56:06 +0800

    Rename config

--- shuffle.py
+++ shuffle.py
@@ -1,7 +1,7 @@
 import json
 from discord.ext import commands
 
-CONFIG_FN = 'config.json'
+CONFIG_FN = 'shuffle.json'
 config = json.loads(open(CONFIG_FN, 'r').read())
 
 print(config['metadata']['token'])
